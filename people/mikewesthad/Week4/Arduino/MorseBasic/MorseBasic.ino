3646629bbbed62481c77fe68f8fe54bf5ec3c4bc
Michael Hadley <michaelwesthadley@gmail.com>
2013-09-17 12:48:41 -0500

    Added comments to MorseBasic

--- people/mikewesthad/Week4/Arduino/MorseBasic/MorseBasic.ino
+++ people/mikewesthad/Week4/Arduino/MorseBasic/MorseBasic.ino
@@ -3,11 +3,20 @@ float dashLength;
 float letterSpaceLength;
 float wordSpaceLength;
 
+// What God Hath Wrought in morse code where ' ' is a space, '>' indicates a new letter and '|' indicates a new word
 char morseString[] = ". - ->. . . .>. ->-|- - .>.  .>- . .|. . . .>. ->->. . . .|. - ->.  . .>.  .>. . ->- - .>. . . .>-";
 
 int led = 13;
 
 void setup() {
+  
+  // Explanation of American Morse Code from: http://w1tp.com/percode.htm
+  //  Dot is basic unit of time
+  //  Dash is three dits
+  //  Space between dots and dashes within characters is one dot
+  //  Space between letters is 3 dots
+  //  Space between words is 7 dots
+  
   dotLength = 0.25;
   dashLength = 3 * dotLength;
   letterSpaceLength = 3 * dotLength;

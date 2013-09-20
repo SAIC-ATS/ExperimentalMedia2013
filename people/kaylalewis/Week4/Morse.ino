fd4d528cd3bf4ade5d9a56c22ea051524cf4d06d
kaylalewis <klewis6@artic.edu>
2013-09-19 22:54:30 -0500

    Git is more confusing than the assignment.

---
+++ people/kaylalewis/Week4/Morse.ino
@@ -0,0 +1,290 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+ 
+  This example code is in the public domain.
+ */
+//.-- .... .- - .... .- - .... / --. --- -.. / .-- .-. --- ..- --. .... -
+ 
+// Pin 13 has an LED connected on most Arduino boards.
+// give it a name:
+int ledPin = 13;
+int dit = 500;
+int dah = dit*3;
+int words = dit*7;
+
+// the setup routine runs once when you press reset:
+void setup() {               
+  pinMode(ledPin, OUTPUT);     
+}
+
+//WHAT W
+void loop() {
+  digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(dit);               // wait for a second
+  digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
+  delay(dit);              // wait for a second
+  digitalWrite(ledPin, HIGH);
+  delay(dah);
+  digitalWrite(ledPin, LOW);
+  delay(dit);
+  digitalWrite(ledPin, HIGH);
+  delay(dah);
+  digitalWrite(ledPin, LOW);
+  delay(dah);
+  //H
+  digitalWrite(ledPin, HIGH);
+  delay(dit);
+  digitalWrite(ledPin,LOW);
+  delay(dit);
+  digitalWrite(ledPin,HIGH);
+  delay(dit);
+  digitalWrite(ledPin,LOW);
+  delay(dit);
+  digitalWrite(ledPin,HIGH);
+  delay(dit);
+  digitalWrite(ledPin,LOW);
+  delay(dit);
+  digitalWrite(ledPin,HIGH);
+  delay(dit);
+  digitalWrite(ledPin,LOW);
+  delay(dah);
+  //A
+  digitalWrite(ledPin, HIGH);
+  delay (dit);
+  digitalWrite(ledPin,LOW);
+  delay (dit);
+  digitalWrite(ledPin, HIGH);
+  delay (dah);
+  digitalWrite(ledPin,LOW);
+  delay (dah);
+  //T
+  digitalWrite(ledPin, HIGH);
+  delay (dah);
+  digitalWrite(ledPin,LOW);
+  delay (words);
+  
+  //HATH H
+  digitalWrite(ledPin, HIGH);
+  delay(dit);
+  digitalWrite(ledPin,LOW);
+  delay(dit);
+  digitalWrite(ledPin,HIGH);
+  delay(dit);
+  digitalWrite(ledPin,LOW);
+  delay(dit);
+  digitalWrite(ledPin,HIGH);
+  delay(dit);
+  digitalWrite(ledPin,LOW);
+  delay(dit);
+  digitalWrite(ledPin,HIGH);
+  delay(dit);
+  digitalWrite(ledPin,LOW);
+  delay(dah);
+  
+  //A
+  digitalWrite(ledPin, HIGH);
+  delay (dit);
+  digitalWrite(ledPin,LOW);
+  delay (dit);
+  digitalWrite(ledPin, HIGH);
+  delay (dah);
+  digitalWrite(ledPin,LOW);
+  delay (dah);
+  
+  //T
+  digitalWrite(ledPin, HIGH);
+  delay (dah);
+  digitalWrite(ledPin,LOW);
+  delay (dah);
+  //H
+   digitalWrite(ledPin, HIGH);
+  delay(dit);
+  digitalWrite(ledPin,LOW);
+  delay(dit);
+  digitalWrite(ledPin,HIGH);
+  delay(dit);
+  digitalWrite(ledPin,LOW);
+  delay(dit);
+  digitalWrite(ledPin,HIGH);
+  delay(dit);
+  digitalWrite(ledPin,LOW);
+  delay(dit);
+  digitalWrite(ledPin,HIGH);
+  delay(dit);
+  digitalWrite(ledPin,LOW);
+  delay(words);
+  
+  //GOD G
+  digitalWrite(ledPin, HIGH);
+  delay (dah);
+  digitalWrite(ledPin,LOW);
+  delay(dit);
+  digitalWrite(ledPin, HIGH);
+  delay (dah);
+  digitalWrite(ledPin,LOW);
+  delay(dit);
+  digitalWrite(ledPin, HIGH);
+  delay (dit);
+  digitalWrite(ledPin,LOW);
+  delay(dah);
+  
+  //O
+  digitalWrite(ledPin, HIGH);
+  delay (dah);
+  digitalWrite(ledPin,LOW);
+  delay(dit);
+  digitalWrite(ledPin, HIGH);
+  delay (dah);
+  digitalWrite(ledPin,LOW);
+  delay(dit);
+  digitalWrite(ledPin, HIGH);
+  delay (dah);
+  digitalWrite(ledPin,LOW);
+  delay(dah);
+  
+  //D
+  digitalWrite(ledPin, HIGH);
+  delay (dah);
+  digitalWrite(ledPin,LOW);
+  delay(dit);
+  digitalWrite(ledPin, HIGH);
+  delay (dit);
+  digitalWrite(ledPin,LOW);
+  delay(dit);
+  digitalWrite(ledPin, HIGH);
+  delay (dit);
+  digitalWrite(ledPin,LOW);
+  delay(words);
+  
+  //WROUGHT W
+  digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(dit);               // wait for a second
+  digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
+  delay(dit);              // wait for a second
+  digitalWrite(ledPin, HIGH);
+  delay(dah);
+  digitalWrite(ledPin, LOW);
+  delay(dit);
+  digitalWrite(ledPin, HIGH);
+  delay(dah);
+  digitalWrite(ledPin, LOW);
+  delay(dah);
+  
+  //R
+  digitalWrite(ledPin, HIGH);
+  delay(dit);
+  digitalWrite(ledPin, LOW);
+  delay(dit);
+  digitalWrite(ledPin, HIGH);
+  delay(dah);
+  digitalWrite(ledPin, LOW);
+  delay(dit);
+  digitalWrite(ledPin, HIGH);
+  delay(dit);
+  digitalWrite(ledPin, LOW);
+  delay(dah);
+  
+  //O
+  digitalWrite(ledPin, HIGH);
+  delay (dah);
+  digitalWrite(ledPin,LOW);
+  delay(dit);
+  digitalWrite(ledPin, HIGH);
+  delay (dah);
+  digitalWrite(ledPin,LOW);
+  delay(dit);
+  digitalWrite(ledPin, HIGH);
+  delay (dah);
+  digitalWrite(ledPin,LOW);
+  delay(dah);
+  
+  //U
+  digitalWrite(ledPin, HIGH);
+  delay (dit);
+  digitalWrite(ledPin,LOW);
+  delay(dit);
+  digitalWrite(ledPin, HIGH);
+  delay (dit);
+  digitalWrite(ledPin,LOW);
+  delay(dit);
+  digitalWrite(ledPin, HIGH);
+  delay (dah);
+  digitalWrite(ledPin,LOW);
+  delay(dah);
+  
+  //G
+  digitalWrite(ledPin, HIGH);
+  delay (dah);
+  digitalWrite(ledPin,LOW);
+  delay(dit);
+  digitalWrite(ledPin, HIGH);
+  delay (dah);
+  digitalWrite(ledPin,LOW);
+  delay(dit);
+  digitalWrite(ledPin, HIGH);
+  delay (dit);
+  digitalWrite(ledPin,LOW);
+  delay(dah);
+  
+  //H
+  digitalWrite(ledPin, HIGH);
+  delay(dit);
+  digitalWrite(ledPin,LOW);
+  delay(dit);
+  digitalWrite(ledPin,HIGH);
+  delay(dit);
+  digitalWrite(ledPin,LOW);
+  delay(dit);
+  digitalWrite(ledPin,HIGH);
+  delay(dit);
+  digitalWrite(ledPin,LOW);
+  delay(dit);
+  digitalWrite(ledPin,HIGH);
+  delay(dit);
+  digitalWrite(ledPin,LOW);
+  delay(dah);
+  
+   //T
+  digitalWrite(ledPin, HIGH);
+  delay (dah);
+  digitalWrite(ledPin,LOW);
+  delay (words);
+  
+  //?
+  digitalWrite(ledPin, HIGH);
+  delay (dit);
+  digitalWrite(ledPin,LOW);
+  delay (dit);
+  digitalWrite(ledPin, HIGH);
+  delay (dit);
+  digitalWrite(ledPin,LOW);
+  delay (dit);
+  digitalWrite(ledPin, HIGH);
+  delay (dah);
+  digitalWrite(ledPin,LOW);
+  delay (dit);
+  digitalWrite(ledPin, HIGH);
+  delay (dah);
+  digitalWrite(ledPin,LOW);
+  delay (words);
+  
+  digitalWrite(ledPin,LOW);
+  delay (words);
+  
+  
+  
+}
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+

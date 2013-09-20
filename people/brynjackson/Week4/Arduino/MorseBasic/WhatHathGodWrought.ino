8f6b529e5dc8d050a2ece8a2d0c09d8b8dd9028e
Bryn Jackson <c.bryn.jackson@gmail.com>
2013-09-19 23:31:34 -0500

    Frustrated

---
+++ people/brynjackson/Week4/Arduino/MorseBasic/WhatHathGodWrought.ino
@@ -0,0 +1,251 @@
+int led = 13;
+int dit = 100;
+int dah = 300;
+int dum = 700;
+
+void setup()
+
+{
+  
+  pinMode(led, OUTPUT);
+  
+}
+void loop () 
+
+{
+  // 'W'
+  digitalWrite(led, HIGH);
+  delay(dit);
+  digitalWrite(led, LOW);
+  delay(dit);
+  digitalWrite(led, HIGH);
+  delay(dah);
+  digitalWrite(led, LOW);
+  delay(dit);
+  digitalWrite(led, HIGH);
+  delay(dah);
+  digitalWrite(led, LOW);
+  delay(dah);
+  
+  // 'H'
+  digitalWrite(led, HIGH);
+  delay(dit);
+  digitalWrite(led, LOW);
+  delay(dit);
+  digitalWrite(led, HIGH);
+  delay(dit);
+  digitalWrite(led, LOW);
+  delay(dit);
+  digitalWrite(led, HIGH);
+  delay(dit);
+  digitalWrite(led, LOW);
+  delay(dit);
+  digitalWrite(led, HIGH);
+  delay(dit);
+  digitalWrite(led, LOW);
+  delay(dah);
+  
+  // 'A'
+  digitalWrite(led, HIGH);
+  delay(dit);
+  digitalWrite(led, LOW);
+  delay(dit);
+    digitalWrite(led, HIGH);
+  delay(dah);
+    digitalWrite(led, LOW);
+  delay(dah);
+  
+  // 'T'
+    digitalWrite(led, HIGH);
+  delay(dah);
+    digitalWrite(led, LOW);
+  delay(dum);
+  
+  // 'H'
+    digitalWrite(led, HIGH);
+  delay(dit);
+  digitalWrite(led, LOW);
+  delay(dit);
+  digitalWrite(led, HIGH);
+  delay(dit);
+  digitalWrite(led, LOW);
+  delay(dit);
+  digitalWrite(led, HIGH);
+  delay(dit);
+  digitalWrite(led, LOW);
+  delay(dit);
+  digitalWrite(led, HIGH);
+  delay(dit);
+  digitalWrite(led, LOW);
+  delay(dah);
+  
+  // 'A'
+    digitalWrite(led, HIGH);
+  delay(dit);
+  digitalWrite(led, LOW);
+  delay(dit);
+    digitalWrite(led, HIGH);
+  delay(dah);
+    digitalWrite(led, LOW);
+  delay(dah);
+  
+  // 'T'
+      digitalWrite(led, HIGH);
+  delay(dah);
+    digitalWrite(led, LOW);
+  delay(dum);
+  
+  // 'H'
+    digitalWrite(led, HIGH);
+  delay(dit);
+  digitalWrite(led, LOW);
+  delay(dit);
+  digitalWrite(led, HIGH);
+  delay(dit);
+  digitalWrite(led, LOW);
+  delay(dit);
+  digitalWrite(led, HIGH);
+  delay(dit);
+  digitalWrite(led, LOW);
+  delay(dit);
+  digitalWrite(led, HIGH);
+  delay(dit);
+  digitalWrite(led, LOW);
+  delay(dum);
+
+  // 'G'
+  digitalWrite(led, HIGH);
+  delay(dah);
+  digitalWrite(led, LOW);
+  delay(dit);
+  digitalWrite(led, HIGH);
+  delay(dah);
+  digitalWrite(led, LOW);
+  delay(dit);
+  digitalWrite(led, HIGH);
+  delay(dit);
+  digitalWrite(led, LOW);
+  delay(dah);
+  
+  // 'O'
+  digitalWrite(led, HIGH);
+  delay(dah);
+  digitalWrite(led, LOW);
+  delay(dit);
+  digitalWrite(led, HIGH);
+  delay(dah);
+  digitalWrite(led, LOW);
+  delay(dit);  digitalWrite(led, HIGH);
+  delay(dah);
+  digitalWrite(led, LOW);
+  delay(dah);  
+  
+  // 'D'
+  digitalWrite(led, HIGH);
+  delay(dah);
+  digitalWrite(led, LOW);
+  delay(dit);
+  digitalWrite(led, HIGH);
+  delay(dit);
+  digitalWrite(led, LOW);
+  delay(dit);
+  digitalWrite(led, HIGH);
+  delay(dit);
+  digitalWrite(led, LOW);
+  delay(dum);
+  
+  // 'W'
+  digitalWrite(led, HIGH);
+  delay(dit);
+  digitalWrite(led, LOW);
+  delay(dit);
+  digitalWrite(led, HIGH);
+  delay(dah);
+  digitalWrite(led, LOW);
+  delay(dit);
+  digitalWrite(led, HIGH);
+  delay(dah);
+  digitalWrite(led, LOW);
+  delay(dah);
+
+  // 'R'
+  digitalWrite(led, HIGH);
+  delay(dit);
+  digitalWrite(led, LOW);
+  delay(dit);
+  digitalWrite(led, HIGH);
+  delay(dah); 
+  digitalWrite(led, LOW);
+  delay(dit);  
+  digitalWrite(led, HIGH);
+  delay(dit);
+  digitalWrite(led, LOW);
+  delay(dah);
+  
+  // 'O'
+  digitalWrite(led, HIGH);
+  delay(dah);
+  digitalWrite(led, LOW);
+  delay(dit);
+  digitalWrite(led, HIGH);
+  delay(dah);
+  digitalWrite(led, LOW);
+  delay(dit);  
+  digitalWrite(led, HIGH);
+  delay(dah);
+  digitalWrite(led, LOW);
+  delay(dah);  
+
+  // 'U'
+  digitalWrite(led, HIGH);
+  delay(dit);
+  digitalWrite(led, LOW);
+  delay(dit);
+  digitalWrite(led, HIGH);
+  delay(dit);
+  digitalWrite(led, LOW);
+  delay(dit);
+   digitalWrite(led, HIGH);
+  delay(dah);
+  digitalWrite(led, LOW);
+  delay(dah);
+  
+  // 'G'
+    digitalWrite(led, HIGH);
+  delay(dah);
+  digitalWrite(led, LOW);
+  delay(dit);
+  digitalWrite(led, HIGH);
+  delay(dah);
+  digitalWrite(led, LOW);
+  delay(dit);
+  digitalWrite(led, HIGH);
+  delay(dit);
+  digitalWrite(led, LOW);
+  delay(dah);
+  
+  // 'H'
+  digitalWrite(led, HIGH);
+  delay(dit);
+  digitalWrite(led, LOW);
+  delay(dit);
+  digitalWrite(led, HIGH);
+  delay(dit);
+  digitalWrite(led, LOW);
+  delay(dit);
+  digitalWrite(led, HIGH);
+  delay(dit);
+  digitalWrite(led, LOW);
+  delay(dit);
+  digitalWrite(led, HIGH);
+  delay(dit);
+  digitalWrite(led, LOW);
+  delay(dah);
+  
+  // 'T'
+      digitalWrite(led, HIGH);
+  delay(dah);
+    digitalWrite(led, LOW);
+  delay(dum);
+  
+}
